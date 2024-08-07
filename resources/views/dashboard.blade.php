ba28decc5f74070a7fed55fe9be68a3166ddc19b
Juan Camilo Developer <juan.camilorodrigez@gmail.com>
2024-08-07 16:20:37 -0500

    Prueba

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,14 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
+        <form method="POST" action="{{ route('logout') }}" x-data>
+                                @csrf
+
+                                <x-dropdown-link href="{{ route('logout') }}"
+                                         @click.prevent="$root.submit();">
+                                    {{ __('Log Out') }}
+                                </x-dropdown-link>
+                            </form>
     </x-slot>
 
     <div class="py-12">
