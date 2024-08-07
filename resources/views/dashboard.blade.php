da7d1ee17063b63399c9d7f6619d374616e51183
Juan Camilo Developer <juan.camilorodrigez@gmail.com>
2024-08-07 16:27:16 -0500

    Prueba 2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,14 +3,10 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-        <form method="POST" action="{{ route('logout') }}" x-data>
-                                @csrf
-
-                                <x-dropdown-link href="{{ route('logout') }}"
-                                         @click.prevent="$root.submit();">
-                                    {{ __('Log Out') }}
-                                </x-dropdown-link>
-                            </form>
+        <form action="{{route('logout')}}" method="post" id="cerrar">
+            @csrf
+        </form>
+        <a  style="cursor: pointer;" onclick="document.getElementById('cerrar').submit()">Cerrar Sesion</a>
     </x-slot>
 
     <div class="py-12">
